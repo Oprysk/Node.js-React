1500d371e657d02d944bcd05e3c2298fd41a2c69
Oprysk <oprusk230992@gmail.com>
2018-01-21 12:38:34 +0200

    create session

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
 const db = require('./db');
 const usersController = require('./controller/users');
 
+const session = require('express-session');
 const app = express();
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello Liubert, I am ready!');
+// Access the session as req.session
+app.get('/', (req, res, next) => {
+    if (req.session.views) {
+        req.session.views++;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<p>views: ' + req.session.views + '</p>');
+        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>');
+        res.end()
+    } else {
+        res.write('<h1>'+'Hello Liubert, I am ready!'+'</h1>');
+        req.session.views = 1;
+        res.end('welcome to the session demo. refresh!')
+    }
 });
 
 app.get('/users', usersController.all);
