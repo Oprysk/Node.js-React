cf49f6c5e1e510202015636e7be0aedc8f44c4fc
lybert <lybert19@gmail.com>
2018-02-04 17:28:56 +0200

    fix greate issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import './main.css'
+import './main.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -8,25 +8,24 @@ import thunk from 'redux-thunk'
 import {syncHistoryWithStore} from 'react-router-redux'
 import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
-
 import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
 import Phone from 'containers/phone'
 import Basket from 'containers/basket'
 import Dashboard from 'containers/dashboard'
-
+import './main.css';
 const store = createStore(reducers, composeWithDevTools(
   applyMiddleware(thunk)
-))
+));
 
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Route component={Layout}>
-        <Route path='/' component={Dashboard}/>
+        <Route store={store} path='/' component={Dashboard}/>
         <Route path='/phones' component={Phones}/>
         <Route path='/categories/:id' component={Phones}/>
       </Route>
