9baed40c6ab13c195bec9798d72d76a045dddfb0
vyacheslav <oprusk230992@gmail.com>
2018-03-30 17:36:14 +0300

    add body-parser

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const tweetsController = require('./timeline-controller');
 const app = express();
 
-const tweetsController = require('./timeline-controller');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function (req, res, next) {
 
