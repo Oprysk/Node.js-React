7f8587e0997bf2682d39f890630481f48aaf91b0
liubert <lybert19@gmail.com>
2018-03-30 16:29:46 +0300

    one more commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,11 +10,7 @@ import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
 import reducers from 'reducers'
 import Layout from 'containers/layout'
-import Phones from 'containers/phones'
-import Phone from 'containers/phone'
-import Basket from 'containers/basket'
 import Dashboard from 'containers/dashboard'
-import Login from 'containers/login'
 import NotFound from 'containers/errorPages/notFound'
 
 import './main.css';
@@ -28,18 +24,9 @@ const history = syncHistoryWithStore(browserHistory, store);
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-        <Route path='/' component={Login} />
       <Route component={Layout}>
-        <Route path='/dashboard' component={Dashboard}/>
-        {/*<Route path='/phones' component={Phones}/>*/}
-        {/*<Route path='/categories/:id' component={Phones}/>*/}
+        <Route path='/' component={Dashboard}/>
       </Route>
-
-        {/*<Route path='/login' component={Login}/>*/}
-      {/*<Route path='/phones/:id' component={Phone}/>*/}
-      {/*<Route path='/basket' component={Basket} />*/}
-
-
         <Route path='*' component={NotFound} />
     </Router>
   </Provider>,
